68906a4794d577627e5e3d2ed7e69ac3800774ba
Taha Dostifam <mr.tahadostifam@gmail.com>
2021-09-23 11:56:16 +0330

    fileuploader styles finished

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -7,4 +7,16 @@ document.addEventListener("turbolinks:load", function() {
 		element: document.querySelector(".mde_textarea"),
 		hideIcons: ["fullscreen", "side-by-side", "guide"],
 	})
+
+	const fileuploadDiv = document.querySelector('.fileupload')
+	fileuploadDiv.querySelector('input').onchange = (e) => {
+	  const file = fileuploadDiv.querySelector('input').files[0]
+	  const imageTag = fileuploadDiv.querySelector('.image img')
+	  if (file) {
+	  	const imageTag = fileuploadDiv.querySelector('.image img')
+	  	imageTag.src = URL.createObjectURL(file)
+
+	  	fileuploadDiv.querySelector('.image').style.display = 'block'
+	  }
+	}
 })
