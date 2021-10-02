6a1777e31a8647e34fc31d2103a0b04003d54bb1
Taha Dostifam <mr.tahadostifam@gmail.com>
2021-10-02 22:15:54 +0330

    u

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,34 @@
 class ArticlesController < ApplicationController
+  before_action :need_owner_access, only: [
+    
+  ]
+  before_action :need_admin_access, only: [
+    :confirm_delete_article
+  ]
+
   def index
     @articles = Article.where(published: true, draft: false)
   end
 
   def show
     @article = Article.find_by(slug: params[:slug])
+  end
+
+  def confirm_delete_article
+
+  end
+
+  private
+
+  def need_owner_access
+    unless session[:user].is_owner?
+      redirect_to '/503'
+    end
+  end
+
+  def need_admin_access
+    unless session[:user].is_admin?
+      redirect_to '/503'
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-require('./prism')
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
