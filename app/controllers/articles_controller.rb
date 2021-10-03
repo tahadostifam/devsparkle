09032167314f1e287b27be5c8ad9c2a9528a3e93
Taha Dostifam <mr.tahadostifam@gmail.com>
2021-10-03 11:28:37 +0330

    submit_delete_article finished.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
     
   ]
   before_action :need_admin_access, only: [
-    :confirm_delete_article
+    :confirm_delete_article,
+    :submit_delete_article
   ]
 
   def index
@@ -12,12 +13,39 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find_by(slug: params[:slug])
+    if session[:user] != nil
+      if session[:user][:id] == @article.user_id || session[:user][:is_owner]
+        render :show, :locals => { :show_actions => true }
+      else
+        render :show, :locals => { :show_actions => false }
+      end
+    else
+      render :show, :locals => { :show_actions => false }
+    end
   end
 
   def confirm_delete_article
     @article = Article.find_by(slug: params[:slug])
     unless @article.present?
       redirect_to action: :index
+    end
+  end
+
+  def submit_delete_article
+    slug = params[:slug]
+    art = Article.find_by(slug: slug)
+    if art.present?
+      if session[:user][:is_owner]
+        art.destroy
+        redirect_to :index
+      elsif art.user_id == session[:user][:id]
+        art.destroy
+        redirect_to :index
+      else
+        redirect_to :index
+      end
+    else
+      redirect_to :index
     end
   end
 
