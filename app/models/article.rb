07b98e3ab6faa4a02fef572884e2e1d44770f986
Taha Dostifam <mr.tahadostifam@gmail.com>
2021-10-11 18:34:21 +0330

    code comments removed

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
         :path => "/public_html/:attachment/:id/:style/:filename",
         :url => "http://zedxgroup.ir/:attachment/:id/:style/:filename",
         :ftp_servers => [
-            { 
+            {   
                 :host => ENV["FTP_HOST"],
                 :user => ENV["FTP_USER"],
                 :password => ENV["FTP_PASS"],
