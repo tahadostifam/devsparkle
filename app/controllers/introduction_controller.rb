09ade850d4a926291a6a773378bbb17c474c1aef
Taha Dostifam <mr.tahadostifam@gmail.com>
2021-10-17 18:05:34 +0330

    popular articles section in index page finished

--- app/controllers/introduction_controller.rb
+++ app/controllers/introduction_controller.rb
@@ -1,4 +1,9 @@
 class IntroductionController < ApplicationController
+    def index
+        @popular_articles = 
+            Article.left_joins(:likes).group(:article_id).order("COUNT(likes.article_id) DESC").limit(20)
+    end
+
     def about_us
         @setting = Setting.first
     end
