4650bbff53683db53486b653b40289893a369eb2
Taha Dostifam <mr.tahadostifam@gmail.com>
2021-10-27 16:24:36 +0330

    paperclip added course_episode

--- app/models/course_episode.rb
+++ app/models/course_episode.rb
@@ -1,3 +1,20 @@
 class CourseEpisode < ApplicationRecord
     belongs_to :course
+
+    has_attached_file :video_file,
+    {
+        :storage => :ftp,
+        :path => ENV["FTP_UPLOAD_PATH"],
+        :url => ENV["FTP_DOWNLOAD_URL"],
+        :ftp_servers => [
+            {   
+                :host => ENV["FTP_HOST"],
+                :user => ENV["FTP_USER"],
+                :password => ENV["FTP_PASS"],
+                passive: true
+            }
+        ],
+        :ftp_ignore_failing_connections => true,
+        :ftp_keep_empty_directories => true
+    }
 end
