4c5ba95b6950e0ac78d0c1760e8721b20623406b
Taha Dostifam <mr.tahadostifam@gmail.com>
2021-11-01 13:28:25 +0330

    bug fix: search section sql-query fixed [its working right]

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,13 @@ class SearchController < ApplicationController
   end
 
   def submit_q
-    @search_in_articles = Article.where('header LIKE ?', "%#{params['search_q']}%")
-    @search_in_courses = Course.where('header LIKE ?', "%#{params['search_q']}%")
+    @search_result = ActiveRecord::Base.connection.execute("""
+      SELECT * FROM articles 
+        WHERE header LIKE '%#{params['search_q']}%'
+      union all
+      SELECT * FROM courses 
+        WHERE header LIKE '%#{params['search_q']}%'
+    """)
     render 'search/q'
   end
 end
